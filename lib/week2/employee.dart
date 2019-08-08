d9ab64710a00b58987488359d04339946f574324
sridr8 <saisuhas@saimail.com>
2019-08-08 13:34:30 +0530

    Dart Object oriented examples.

--- lib/week2/employee.dart
+++ lib/week2/employee.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 class Employee {
   String name;
   String id;
@@ -9,11 +11,36 @@ class Employee {
     this.gender = gender;
     this.designation = designation;
   }
-  String empDetails(String id) =>
-      'name: $name \n Gender: $gender \n Designation; $designation';
+  String empDetails() =>
+      'name: $this.name \n ID: $this.id\n Gender: $this.gender \n Designation; $this.designation';
 }
 
 void main() {
-  Employee emp1 = new Employee('Srinivas', '12345', 'Male', 'Engineer');
-  print(emp1.empDetails('12345'));
+  bool isFound = false;
+  Employee emp1 = new Employee('Raghu', '11111', 'Male', 'Manager');
+  Employee emp2 = new Employee('Murali', '22222', 'Male', 'Administrator');
+  Employee emp3 = new Employee('Sudheer', '33333', 'Male', 'Architect');
+  Employee emp4 = new Employee('Siva', '44444', 'Male', 'Engineer');
+  Employee emp5 = new Employee('Pavan', '55555', 'Male', 'Senior Engineer');
+
+  var employeeList = List<Employee>();
+  employeeList.addAll([emp1, emp2, emp3, emp4, emp5]);
+  print('Enter employee ID: ');
+  String empID = stdin.readLineSync();
+
+  for (Employee emp in employeeList) {
+    if (empID == emp.id) {
+      print('Employee details: ');
+      print('*****************************');
+      print(' Name: ${emp.name} \n '
+          'ID: ${emp.id} \n '
+          'Gender: ${emp.gender} \n '
+          'Designation: ${emp.designation}');
+      print('*****************************');
+      isFound = true;
+    }
+  }
+  if (isFound == false) {
+    print('Employee ID not found!');
+  }
 }
